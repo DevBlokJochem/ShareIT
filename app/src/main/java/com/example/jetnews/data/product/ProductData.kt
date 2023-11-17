323cae17bdbe65a98a281b9106b2dd5eedbe11f4
Jochem <jochemminecraftyt@gmail.com>
2023-11-17 11:45:09 +0100

    Create ProductData.kt

---
+++ app/src/main/java/com/example/jetnews/data/product/ProductData.kt
@@ -0,0 +1,19 @@
+package com.example.jetnews.data.product
+
+import java.util.UUID
+
+object ProductData {
+
+    val products: ArrayList<Product> = arrayListOf(Product(UUID.randomUUID(), "test1", null, "", " ", 50.0, "ema lkml"))
+
+}
+
+data class Product(
+    val id: UUID,
+    var name: String,
+    var photo: String?,
+    var description: String,
+    val location: String,
+    var price: Double,
+    val seller: String
+)
